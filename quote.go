9ee25bd58ebb6feb85bd711d27ae6ed0d3542c86
William Antonelli <code@nanohard.net>
2021-03-14 11:58:06 -0500

    fix: print one quote, really fixed this time

--- quote.go
+++ quote.go
@@ -32,14 +32,8 @@ func commandQuote(channel string, chUser *twitch.User, args ...string) {
 		return
 	}
 
-	number, err := strconv.Atoi(args[0])
-
-	log.Println(err)
-	log.Println(number)
-	log.Println(length)
-
 	// If user trying to print 1 quote
-	if number, err := strconv.Atoi(args[0]); err == nil && length == 1 && number <= length {
+	if number, err := strconv.Atoi(args[0]); err == nil && length == 1 && number <= len(ch.Quotes) {
 		say(channel, ch.Quotes[number-1])
 		return
 	}
